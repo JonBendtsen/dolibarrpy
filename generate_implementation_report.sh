80931fa2ea349155af81dc74a77947e9e4d955f2
Jon Bendtsen <xcodeauthor@jonb.dk>
2024-04-30 18:11:38 +0200

    ignoring lines we don't need

--- generate_implementation_report.sh
+++ generate_implementation_report.sh
@@ -7,7 +7,7 @@ IFS="
 "
 
 echo -e "#COUNT\tENDPOINT" > "implementation_status.csv"
-for ENDPOINT in $( grep -ir " /" "${INPUT}" | grep -E "delete|get|patch|post|put" | cut -d":" -f2 | sed -e "s/^ *//" ); do
+for ENDPOINT in $( grep -ir " /" "${INPUT}" | grep -v "base url" | grep -v " / " | grep -E "delete|get|patch|post|put" | cut -d":" -f2 | sed -e "s/^ *//" ); do
     FOUND=$( grep -c -e "@endpoint '${ENDPOINT}'$" dolibarrpy/Dolibarrpy.py )
     echo -e "${FOUND}\t${ENDPOINT}" >> "implementation_status.csv"
 done
